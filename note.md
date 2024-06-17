ef16cc872e7af4087db535e1c50ba33d6f83b025
Fomin Alexey <fomin.alexey@outlook.com>
2024-06-17 15:09:21 +0300

    v1.0.21

--- note.md
+++ note.md
@@ -1,1 +1,1 @@
-git add *;git commit -am "v1.0.20";git tag v1.0.20;git push origin v1.0.20;git push
+git add *;git commit -am "v1.0.21";git tag v1.0.21;git push origin v1.0.21;git push
--- utils/files/dir.go
+++ utils/files/dir.go
@@ -3,19 +3,16 @@ package files
 import (
 	"os"
 	"path/filepath"
-
-	"github.com/LittleDrongo/fmn-lib/exception"
 )
 
 func MakeDirIfIsNotExist(path string) error {
 
 	dir := filepath.Dir(path)
 
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		err := os.MkdirAll(dir, 0755)
-		return exception.DropUp(err, "Ошибка при создании папки:")
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
 	}
-
 	return nil
 
 }
