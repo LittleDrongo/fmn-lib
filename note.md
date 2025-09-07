bac0e990859e6a14e3cc107c0905655eb0898eec
Drongo <fomin.alexey@outlook.com>
2025-09-07 12:47:10 +0300

    v1.0.38

--- note.md
+++ note.md
@@ -1,1 +1,1 @@
-git add *;git commit -am "v1.0.37";git tag v1.0.37;git push origin v1.0.37;git push
+git add *;git commit -am "v1.0.38";git tag v1.0.38;git push origin v1.0.38;git push
--- utils/files/file.go
+++ utils/files/file.go
@@ -7,8 +7,15 @@ import (
 
 func CreateFileIfIsnotExist(filepath string) (*os.File, error) {
 
-	if _, err := os.Stat(filepath); err == nil {
-		return nil, fmt.Errorf("файл уже существует: %s", filepath)
+	if info, err := os.Stat(filepath); err == nil {
+		if !info.IsDir() {
+			file, err := os.Open(filepath)
+			if err != nil {
+				return nil, err
+			}
+			return file, nil
+		}
+
 	} else if !os.IsNotExist(err) {
 		return nil, fmt.Errorf("ошибка при проверке существования файла: %v", err)
 	}
