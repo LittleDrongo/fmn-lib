2e4f35f7aef4ae7c752d84578a8b5e9fa6f45d40
Drongo <fomin.alexey@outlook.com>
2025-09-10 13:40:27 +0300

    v1.0.39

--- utils/elements/elements.go
+++ utils/elements/elements.go
@@ -23,3 +23,12 @@ func IsLastElement[T any](index int, arr ...T) bool {
 	lastIndex := len(arr)
 	return index == lastIndex
 }
+
+func Contains[T comparable](slice []T, item T) bool {
+	for _, v := range slice {
+		if v == item {
+			return true
+		}
+	}
+	return false
+}
---
+++ utils/yam/yam_protected.go
@@ -0,0 +1,44 @@
+package yam
+
+import "fmt"
+
+func ImportProtectedStruct[S any](
+	filepath string,
+	tmpFactory func() any,
+	apply func(tmp any) (S, error),
+) (S, error) {
+	var zero S
+
+	tmp := tmpFactory()
+	if tmp == nil {
+		return zero, fmt.Errorf("tmpFactory is nil")
+	}
+
+	if err := Import(filepath, tmp); err != nil {
+		return zero, err
+	}
+
+	out, err := apply(tmp)
+	if err != nil {
+		return zero, err
+	}
+	return out, nil
+}
+
+/*
+ */
+func ExportProtectedStruct[S any](
+	data S,
+	filepath string,
+	buildTmp func(src S) (any, error),
+) error {
+	tmp, err := buildTmp(data)
+	if err != nil {
+		return err
+	}
+	if tmp == nil {
+		return fmt.Errorf("buildTmp is nil")
+	}
+
+	return Export(tmp, filepath)
+}
